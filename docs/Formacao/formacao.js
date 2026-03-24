12dd8aa6ecf69c72703a9212ebb0bbc6325335a2
VictorCardosoPDev <vic.koopa45@gmail.com>
2026-03-24 00:19:13 -0300

    feat: remoção de paginas desnecessarias

--- docs/Formacao/formacao.js
+++
@@ -1,6 +0,0 @@
-function toggleProjeto(header) {
-    const conteudo = header.nextElementSibling;
-    const isVisible = conteudo.style.display === "flex";
-    document.querySelectorAll('.formacao-conteudo').forEach(c => c.style.display = 'none');
-    conteudo.style.display = isVisible ? "none" : "flex";
-}
